3cc661dbd914bc602ace57b98740e55c8c36d730
pengxiulin <pengxiulin@luojilab.com>
2016-09-21 17:11:45 +0800

    modify

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-for(var i in process.argv){
-    if(i>1){
-        console.log(process.argv[i]);
+if(process.argv.length>2){
+    for(var i in process.argv){
+        if(i>1){
+            console.log(process.argv[i]);
+        }
     }
+}else{
+    console.log('help');
 }
